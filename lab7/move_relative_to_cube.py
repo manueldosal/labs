a5c733d6b3ea6ea05b097b3e7e80c6e4c8bc0768
Manuel Dosal <madosal@microsoft.com>
2018-04-30 01:09:28 -0700

    Answer lab7 part 5

--- lab7/move_relative_to_cube.py
+++ lab7/move_relative_to_cube.py
@@ -11,7 +11,7 @@ import math
 import time
 import sys
 
-from odometry import cozmo_go_to_pose
+from odometry import cozmo_go_to_pose, my_go_to_pose1
 sys.path.insert(0, '../lab6')
 from pose_transform import get_relative_pose
 
@@ -31,14 +31,25 @@ def move_relative_to_cube(robot: cozmo.robot.Robot):
 		except asyncio.TimeoutError:
 			print("Didn't find a cube")
 
-	desired_pose_relative_to_cube = Pose(0, 100, 0, angle_z=degrees(90))
+	desired_pose_relative_to_cube = Pose(100, 100, 0, angle_z=degrees(225))
 
 	# ####
-	# TODO: Make the robot move to the given desired_pose_relative_to_cube.
+	# Make the robot move to the given desired_pose_relative_to_cube.
 	# Use the get_relative_pose function your implemented to determine the
 	# desired robot pose relative to the robot's current pose and then use
 	# one of the go_to_pose functions you implemented in Lab 6.
 	# ####
+
+	#Get the robot's pose in cube coordinates
+	initialRobotPose = get_relative_pose(robot.pose, cube.pose)
+	print("Robot pose relative to cube coordinates", initialRobotPose)
+
+	#Get the target pose in robot's coordinates
+	targetRobotPose = get_relative_pose(desired_pose_relative_to_cube, initialRobotPose)
+	print("Target robot pose relative to current robot coordinates", targetRobotPose)
+
+	#Drive to the pose
+	my_go_to_pose1(robot, targetRobotPose.position.x, targetRobotPose.position.y, targetRobotPose.rotation.angle_z.degrees)
 
 
 if __name__ == '__main__':

5f5ede826224b8898dce57d858fdb45efb08af8a
Manuel Dosal <madosal@microsoft.com>
2018-05-19 00:29:35 -0700

    Creating a loop to vote for symbol after several iterations

--- lab11/cozmoImageRecognition.py
+++ lab11/cozmoImageRecognition.py
@@ -3,6 +3,7 @@
 import asyncio
 import sys
 import time
+import operator
 
 import cv2
 import numpy as np
@@ -37,41 +38,57 @@ async def run(robot: cozmo.robot.Robot):
 
     try:
         while True:
-            print("Getting cozmo's image")
-            #get camera image
-            event = await robot.world.wait_for(cozmo.camera.EvtNewRawCameraImage, timeout=30)
-            print("Image received")
 
-            #convert camera image to opencv format
-            opencv_image = cv2.cvtColor(np.asarray(event.image), cv2.COLOR_RGB2GRAY)
-            print("Converted to opencv format")
+            symbolCount = {
+                "drone" : 0,
+                "hands" : 0,
+                "inspection" : 0,
+                "order" : 0,
+                "place" : 0,
+                "plane" : 0,
+                "truck" : 0,
+                "none" : 0,
+            }
 
-            #Find the symbol
-            imageInstance = img_clf.extract_image_features([opencv_image])
-            print("Generated instance by extracting features")
-            symbolsList = savedClassifier.predict(imageInstance)
-            print("Predicted label:", symbolsList[0])
-            symbol = symbolsList[0]
-            print("Image has been classified")
+            for i in range(10):
+                print("Getting cozmo's image")
+                #get camera image
+                event = await robot.world.wait_for(cozmo.camera.EvtNewRawCameraImage, timeout=30)
+                print("Image received")
+
+                #convert camera image to opencv format
+                opencv_image = cv2.cvtColor(np.asarray(event.image), cv2.COLOR_RGB2GRAY)
+                print("Converted to opencv format")
+
+                #Find the symbol
+                imageInstance = img_clf.extract_image_features([opencv_image])
+                print("Generated instance by extracting features")
+                symbolsList = savedClassifier.predict(imageInstance)
+                print("Predicted label:", symbolsList[0])
+                symbol = symbolsList[0]
+                symbolCount[symbol] = symbolCount[symbol] + 1
+
+            print("symbolCount:", symbolCount)
+            finalSymbol = max(symbolCount.items(), key=operator.itemgetter(1))[0]
 
             # Make cozmo tell what he's seeing
-            await robot.say_text(symbol).wait_for_completed()
+            await robot.say_text(finalSymbol).wait_for_completed()
 
-            if symbol == "drone":
+            if finalSymbol == "drone":
                 await robot.play_anim(name="anim_bored_01").wait_for_completed()
-            elif symbol == "hands":
+            elif finalSymbol == "hands":
                 await robot.play_anim(name="anim_poked_giggle").wait_for_completed()
-            elif symbol == "inspection":
+            elif finalSymbol == "inspection":
                 await robot.play_anim(name="anim_pounce_success_02").wait_for_completed()
-            elif symbol == "order":
+            elif finalSymbol == "order":
                 await robot.play_anim(name="anim_bored_event_02").wait_for_completed()
-            elif symbol == "place":
+            elif finalSymbol == "place":
                 await robot.play_anim(name="anim_bored_event_03").wait_for_completed()
-            elif symbol == "plane":
+            elif finalSymbol == "plane":
                 await robot.play_anim(name="anim_petdetection_cat_01").wait_for_completed()
-            elif symbol == "truck":
+            elif finalSymbol == "truck":
                 await robot.play_anim(name="anim_petdetection_dog_03").wait_for_completed()
-            elif symbol == "none":
+            elif finalSymbol == "none":
                 await robot.play_anim(name="anim_reacttoface_unidentified_02").wait_for_completed()
             
             print("Putting Cozmo in a idle position")
